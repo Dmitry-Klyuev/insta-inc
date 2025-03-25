f6608741ba27853a6c1ebbef40338481f577bc78
Dmitry-Klyuev <i6169696@gmail.com>
2025-03-25 16:40:42 +0300

    fix mixin style

--- shared/ui/button/Button.tsx
+++ shared/ui/button/Button.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import style from './Button.module.scss';
 
+import style from './Button.module.scss';
 
 type ButtonTypes = 'primary' | 'dark' | 'outlined' | 'link';
 
 type ButtonProps = {
-    title: string;
-    variant: ButtonTypes;
-} &  React.ButtonHTMLAttributes<HTMLButtonElement>;
+  title: string;
+  variant: ButtonTypes;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = ({ title, disabled, variant, ...props }: ButtonProps) => {
-    return (
-        <button
-            className={`${style.btn} ${style[variant]}`}
-            disabled={disabled}
-            {...props}
-        >
-            {title}
-        </button>
-    );
+  return (
+    <button
+      className={`${style.btn} ${style[variant]}`}
+      disabled={disabled}
+      {...props}
+    >
+      {title}
+    </button>
+  );
 };

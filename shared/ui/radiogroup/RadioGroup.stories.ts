800d76d630a02c99f4290767566852c80e843b0a
bonadysev_sergey <bonadysev@gmail.com>
2025-04-12 14:34:24 +0300

    TQ-39 feat:fix RadioGroup.stories

--- shared/ui/radiogroup/RadioGroup.stories.ts
+++ shared/ui/radiogroup/RadioGroup.stories.ts
@@ -1,11 +1,11 @@
-import { RadioGroup } from './RadioGroup';
+import { Radio_Group } from './RadioGroup';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'UI/RadioGroup',
-  component: RadioGroup,
+  component: Radio_Group,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
@@ -17,35 +17,36 @@ const meta = {
     // backgroundColor: { control: 'color' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-} satisfies Meta<typeof RadioGroup>;
+} satisfies Meta<typeof Radio_Group>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    // primary: true,
-    // label: 'Button',
+    status: true,
   },
 };
 
-export const Secondary: Story = {
-  // args: {
-  //   label: 'Button',
-  // },
-};
-
-export const Large: Story = {
+export const Active: Story = {
   args: {
-    // size: 'large',
-    // label: 'Button',
+    disable: false,
+    status: true,
+    title: '',
   },
 };
 
-export const Small: Story = {
+export const Focus: Story = {
   args: {
-    // size: 'small',
-    // label: 'Button',
+    status: true,
+    disable: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disable: true,
+    status: true,
   },
 };

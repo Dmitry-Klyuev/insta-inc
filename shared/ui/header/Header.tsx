09014e8be3bb42b0663e9e2147d5edca5d65f5a3
Komandr33 <and.kom2017@gmail.com>
2025-04-30 17:34:48 +0300

    TQ-47: added Link for title

--- shared/ui/header/Header.tsx
+++ shared/ui/header/Header.tsx
@@ -8,7 +8,7 @@ import { Button } from 'shared/ui/button/Button';
 import { SelectLanguageItem } from 'shared/ui/select/LanguageItem';
 import { Select } from 'shared/ui/select/Select';
 
-import s from './Header.module.scss';
+import styles from './Header.module.scss';
 
 type Props = {
   title: string;
@@ -33,12 +33,12 @@ export const Header = ({
   const [language, setLanguage] = useState<string>('English');
 
   return (
-    <header className={s.header}>
-      <Link href="/">
-        <h1 className={s.title}>{title}</h1>
+    <header className={styles.header}>
+      <Link href={'/'} className={styles.link_title}>
+        <h1 className={styles.title}>{title}</h1>
       </Link>
-      <div className={s.content}>
-        <div className={s.language_select}>
+      <div className={styles.content}>
+        <div className={styles.language_select}>
           <Select
             onChange={(value) => setLanguage(value as SelectLanguageItem)}
             selectedLanguage={true}
@@ -46,20 +46,23 @@ export const Header = ({
           />
         </div>
         {isAuth ? (
-          <div className={s.alarm}>
-            <button className={s.notification} onClick={notificationHandler}>
+          <div className={styles.alarm}>
+            <button
+              className={styles.notification}
+              onClick={notificationHandler}
+            >
               <svg viewBox="0 0 24 24" fill="currentColor">
                 <use xlinkHref="/sprite/sprite-icons.svg#outline-bell" />
               </svg>
               {notificationCount > 0 && (
-                <span className={s.notification_badge}>
+                <span className={styles.notification_badge}>
                   {notificationCount}
                 </span>
               )}
             </button>
           </div>
         ) : (
-          <div className={s.login_button}>
+          <div className={styles.login_button}>
             <Button title="Log in" variant="link" onClick={onLogin} />
             <Button title="Sign up" variant="primary" onClick={onSignup} />
           </div>
